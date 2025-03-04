2d3dc9e215daf91885b0049cf7fc4ef084518eb1
talladac <figuracionc@gmail.com>
2025-03-04 16:14:58 +0400

    Finished core requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user for their grade //
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+        
+
+        if (number >= 90)
+        {
+            letter = "A" ;
+        }
+        else if (number >= 80)
+        {
+            letter = "B" ;
+        
+        }
+        else if (number >= 70)
+        {
+            letter = "C" ;
+        }
+        else if (number >= 60)
+        {
+            letter = "D" ;
+        }
+        else 
+        {
+            letter = "F" ;
+        }
+
+        Console.WriteLine($"Your grade letter is {letter}.");
+
+        if (number > 70)
+        {
+            Console.Write("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed. You can do better!");
+        }
     }
 }
